bd14cd6d242aaa2655baf6bf5d858ccf9a970e0f
Anderson de Brito Oliveira <andersonbritooliveiradev@gmail.com>
2021-01-22 16:53:01 +0000

    Create User Object, Validation,Repository, Service

---
+++ src/main/java/io/abo/ppmtool/services/UserService.java
@@ -0,0 +1,13 @@
+package io.abo.ppmtool.services;
+
+import io.abo.ppmtool.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+}
